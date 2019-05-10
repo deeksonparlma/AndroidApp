38b286a8acd21d3f3311f58ed0c413de21becd11
dicksonparlma <dicksonparlma@gmail.com>
2019-05-10 21:52:03 +0300

    attached an onclick listener to the sign up button

--- app/src/main/java/com/epicodus/jobhunt/MainActivity.java
+++ app/src/main/java/com/epicodus/jobhunt/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        msignupButton.setOnClickListener(this);
     }
 
     @Override
