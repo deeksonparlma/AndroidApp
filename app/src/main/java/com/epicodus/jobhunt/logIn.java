a0cb2929aba0014209073fa5b05056f505aa0d27
dicksonparlma <dicksonparlma@gmail.com>
2019-05-10 17:50:22 +0300

    added buttons to the main activity and added a login activity

---
+++ app/src/main/java/com/epicodus/jobhunt/logIn.java
@@ -0,0 +1,13 @@
+package com.epicodus.jobhunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class logIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
