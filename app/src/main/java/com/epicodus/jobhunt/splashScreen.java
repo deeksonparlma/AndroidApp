57c8f37125805a4a46c5f5069f73a59ddeec9619
dicksonparlma <dicksonparlma@gmail.com>
2019-05-10 17:33:31 +0300

    added time limit the splashscreen is to display

--- app/src/main/java/com/epicodus/jobhunt/splashScreen.java
+++ app/src/main/java/com/epicodus/jobhunt/splashScreen.java
@@ -1,5 +1,7 @@
 package com.epicodus.jobhunt;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class splashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run(){
+                        Intent intent = new Intent(getApplicationContext() , MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },1200);
     }
 }
