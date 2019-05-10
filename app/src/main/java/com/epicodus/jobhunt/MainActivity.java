890d98961892d3ecd793ba2873c7a0167fbdbd85
dicksonparlma <dicksonparlma@gmail.com>
2019-05-10 21:46:53 +0300

    made the class implement the methods from onclick interface

--- app/src/main/java/com/epicodus/jobhunt/MainActivity.java
+++ app/src/main/java/com/epicodus/jobhunt/MainActivity.java
@@ -3,17 +3,23 @@ package com.epicodus.jobhunt;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.button) Button mFindRestaurantsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
