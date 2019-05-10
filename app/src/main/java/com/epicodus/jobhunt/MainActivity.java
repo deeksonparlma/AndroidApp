aa75dc02e2fc1d300e58e5fcce79a2ddadcf0bcd
dicksonparlma <dicksonparlma@gmail.com>
2019-05-10 21:45:51 +0300

    binded the signup button

--- app/src/main/java/com/epicodus/jobhunt/MainActivity.java
+++ app/src/main/java/com/epicodus/jobhunt/MainActivity.java
@@ -1,13 +1,19 @@
 package com.epicodus.jobhunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.button) Button mFindRestaurantsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
