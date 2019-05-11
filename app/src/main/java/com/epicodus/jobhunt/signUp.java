50344424ba0d2b04c083a07131608b81179e04d7
dicksonparlma <dicksonparlma@gmail.com>
2019-05-11 09:02:25 +0300

    added a bottom navigation bar

--- app/src/main/java/com/epicodus/jobhunt/signUp.java
+++ app/src/main/java/com/epicodus/jobhunt/signUp.java
@@ -1,13 +1,30 @@
 package com.epicodus.jobhunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class signUp extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class signUp extends AppCompatActivity implements View.OnClickListener{
+@BindView(R.id.button3) Button mLandScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+        mLandScreen.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mLandScreen){
+            Intent intent = new Intent(signUp.this,HomeActivity.class);
+            startActivity(intent);
+        }
     }
 }
