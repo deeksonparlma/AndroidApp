4f642033a0026c55299f184803d828765fd3e234
dicksonparlma <dicksonparlma@gmail.com>
2019-05-11 08:40:25 +0300

    added an onclick functionality to the login button

--- app/src/main/java/com/epicodus/jobhunt/MainActivity.java
+++ app/src/main/java/com/epicodus/jobhunt/MainActivity.java
@@ -11,18 +11,24 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.button) Button msignupButton;
+    @BindView(R.id.button2) Button mloginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         msignupButton.setOnClickListener(this);
+        mloginButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (v == msignupButton) {
             Intent intent = new Intent(MainActivity.this, signUp.class);
+            startActivity(intent);
+        }
+        else if(v == mloginButton){
+            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
             startActivity(intent);
         }
     }
