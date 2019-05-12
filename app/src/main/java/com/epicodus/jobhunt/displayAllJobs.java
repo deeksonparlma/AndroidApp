d00719a8a640c97fc832b5b52219de33b1c8b420
dicksonparlma <dicksonparlma@gmail.com>
2019-05-12 10:29:33 +0300

    created a class to view all jobs and set a click listener to the jobs icon

---
+++ app/src/main/java/com/epicodus/jobhunt/displayAllJobs.java
@@ -0,0 +1,13 @@
+package com.epicodus.jobhunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class displayAllJobs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_all_jobs);
+    }
+}
--- app/src/main/java/com/epicodus/jobhunt/searchByCompany.java
+++ app/src/main/java/com/epicodus/jobhunt/searchByCompany.java
@@ -1,13 +1,30 @@
 package com.epicodus.jobhunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class searchByCompany extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class searchByCompany extends AppCompatActivity implements View.OnClickListener {
+@BindView(R.id.home) ImageView mHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_by_company);
+        ButterKnife.bind(this);
+        mHome.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mHome){
+            Intent intent = new Intent(searchByCompany.this,jobSearch.class);
+            startActivity(intent);
+        }
     }
 }
+
