b3d802e2acb3fe33fcc3cb2f4c895a8fb794607e
dicksonparlma <dicksonparlma@gmail.com>
2019-05-12 10:02:41 +0300

    added onclick listeners on the search options buttons in home screen

--- app/src/main/java/com/epicodus/jobhunt/jobSearch.java
+++ app/src/main/java/com/epicodus/jobhunt/jobSearch.java
@@ -1,13 +1,36 @@
 package com.epicodus.jobhunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class jobSearch extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class jobSearch extends AppCompatActivity implements View.OnClickListener {
+@BindView(R.id.button6) Button mCompany;
+@BindView(R.id.button7) Button mJobName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_search);
+        ButterKnife.bind(this);
+        mCompany.setOnClickListener(this);
+        mJobName.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mCompany){
+            Intent intent = new Intent(jobSearch.this,searchByCompany.class);
+            startActivity(intent);
+        }
+        else if (v == mJobName){
+            Intent intent = new Intent(jobSearch.this,searchByJob.class);
+            startActivity(intent);
+        }
+
     }
 }
