ba91aea418759b42c924ad81fc2dfdbaf305b812
dicksonparlma <dicksonparlma@gmail.com>
2019-05-13 16:15:40 +0300

    imported Grid view

--- app/src/main/java/com/epicodus/jobhunt/displayAllJobs.java
+++ app/src/main/java/com/epicodus/jobhunt/displayAllJobs.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class displayAllJobs extends AppCompatActivity implements View.OnClickListener{
+    GridView gridView;
     @BindView(R.id.home) ImageView mHome;
     @BindView(R.id.jobs) ImageView mJobs;
     @BindView(R.id.chat) ImageView mChat;
