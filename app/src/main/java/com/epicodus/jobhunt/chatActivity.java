f00e1088bddc9d4a9a35010985059f17e3841bdf
dicksonparlma <dicksonparlma@gmail.com>
2019-05-13 08:06:35 +0300

    added navigation control to the navigation bar in chat activity

--- app/src/main/java/com/epicodus/jobhunt/chatActivity.java
+++ app/src/main/java/com/epicodus/jobhunt/chatActivity.java
@@ -1,10 +1,12 @@
 package com.epicodus.jobhunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -14,6 +16,9 @@ public class chatActivity extends AppCompatActivity implements View.OnClickListe
 @BindView(R.id.textView8) TextView mMessage;
 @BindView(R.id.button8) Button mSend;
 @BindView(R.id.editText7) EditText mTyping;
+    @BindView(R.id.home)
+    ImageView mHome;
+    @BindView(R.id.jobs) ImageView mJobs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,8 @@ public class chatActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_chat);
         ButterKnife.bind(this);
         mSend.setOnClickListener(this);
+        mJobs.setOnClickListener(this);
+        mHome.setOnClickListener(this);
     }
 
     @Override
@@ -29,6 +36,16 @@ public class chatActivity extends AppCompatActivity implements View.OnClickListe
             String message =mTyping.getText().toString();
             mMessage.setText(message);
             mTyping.setText("");
+        }
+        else if (v == mJobs){
+            Intent intent = new Intent(chatActivity.this,displayAllJobs.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivityForResult(intent, 0);
+        }
+        else if (v == mHome){
+            Intent intent = new Intent(chatActivity.this,jobSearch.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivityForResult(intent, 0);
         }
     }
 }
