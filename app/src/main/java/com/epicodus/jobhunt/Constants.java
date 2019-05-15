fe95596de20a3cc43317dd62290eb49f853b92de
dicksonparlma <dicksonparlma@gmail.com>
2019-05-15 10:33:24 +0300

    Created a constant class to hold the API's credentials

---
+++ app/src/main/java/com/epicodus/jobhunt/Constants.java
@@ -0,0 +1,7 @@
+package com.epicodus.jobhunt;
+
+public class Constants {
+    public static final String MUSE_TOKEN = BuildConfig.MUSE_TOKEN;
+    public static final String MUSE_BASE_URL = "https://www.themuse.com/api/public/";
+    public static final String JOBS_QUERY_PARAMETER = "jobs";
+}
