959529a3e1978bd86fc480684597d7ad996cfab7
dicksonparlma <dicksonparlma@gmail.com>
2019-05-16 14:24:42 +0300

    added a construcutor to the JobModel class

--- app/src/main/java/com/epicodus/jobhunt/model/JobModel.java
+++ app/src/main/java/com/epicodus/jobhunt/model/JobModel.java
@@ -1,5 +1,17 @@
 package com.epicodus.jobhunt.model;
 
 public class JobModel {
+String mCompany;
+String mlocations;
+String mrefs;
+String mContents;
+String mCategory;
 
+public JobModel(String company,String location,String ref,String contents,String category){
+    this.mCategory = category;
+    this.mCompany =company;
+    this.mContents = contents;
+    this.mrefs = ref;
+    this.mlocations = location;
+    }
 }
