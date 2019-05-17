b267a8e130066dbca12b45170eb38f70a9c1c531
dicksonparlma <dicksonparlma@gmail.com>
2019-05-17 07:58:27 +0300

    added a custom layout to display the jobs in a list

--- app/src/main/java/com/epicodus/jobhunt/constants/Constants.java
+++ app/src/main/java/com/epicodus/jobhunt/constants/Constants.java
@@ -8,4 +8,12 @@ public class Constants {
     public static final String JOBS_QUERY_PARAMETER = "page";
     public static final String PAGE_QUERY_PARAMETER = "descending";
 
+    //chat//
+//    public static final String ACABOT_TOKEN = BuildConfig.MUSE_TOKEN;
+//    public static final String ACABOT_BASE_URL = "https://acobot-brainshop-ai-v1.p.rapidapi.com/get";
+//    public static final String CHAT_QUERY_PARAMETER = "msg";
+//    public static final String BID_QUERY_PARAMETER = "bid";
+//    public static final String UI_ID = "uid";
+
+
 }
---
+++ app/src/main/java/com/epicodus/jobhunt/service/AcobotService.java
@@ -0,0 +1,31 @@
+package com.epicodus.jobhunt.service;
+
+import com.epicodus.jobhunt.constants.Constants;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class AcobotService {
+
+    public static void findRestaurants(String message, String uid,String bid,Callback callback) {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.ACABOT_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.CHAT_QUERY_PARAMETER, message);
+        urlBuilder.addQueryParameter(Constants.BID_QUERY_PARAMETER, bid);
+        urlBuilder.addQueryParameter(Constants.UI_ID, uid);
+        String url = urlBuilder.build().toString();
+
+        //request//
+        Request request = new Request.Builder()
+                .url(url)
+                .header("X-RapidAPI-Key", Constants.ACABOT_TOKEN)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+}
