ca24bac0e162cd27418ead128513e4f2683e1635
dicksonparlma <dicksonparlma@gmail.com>
2019-05-18 12:26:09 +0300

    validated the inputs in sign up activity

--- app/src/main/java/com/epicodus/jobhunt/ui/signUp.java
+++ app/src/main/java/com/epicodus/jobhunt/ui/signUp.java
@@ -3,6 +3,7 @@ package com.epicodus.jobhunt.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +17,10 @@ import butterknife.ButterKnife;
 public class signUp extends AppCompatActivity implements View.OnClickListener{
 @BindView(R.id.button3) Button mLandScreen;
 @BindView(R.id.editText) EditText mUsername;
+@BindView(R.id.editText3) EditText mEmail;
+@BindView(R.id.editText2) EditText mPassword;
+@BindView(R.id.editText4) EditText mConfirmPassword;
+@BindView(R.id.editText5) EditText mTelephone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +33,29 @@ public class signUp extends AppCompatActivity implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         if(v == mLandScreen){
-            String username = mUsername.getText().toString();
-            Toast.makeText(signUp.this, "Sign up successful", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(signUp.this,HomeActivity.class);
-            intent.putExtra("username",username);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-            startActivityForResult(intent, 0);
+            if (mUsername.getText().toString().trim().isEmpty()) {
+                mUsername.setError("invalid Username");
+            }
+            else if(!(Patterns.EMAIL_ADDRESS.matcher(mEmail.getText()).matches())){
+                mEmail.setError("invalid email");
+            }
+            else if(mPassword.getText().toString().length() < 8){
+                mPassword.setError("password is less than 8 characters");
+            }
+            else if(!(mConfirmPassword.getText().toString().matches(mPassword.getText().toString()))){
+                mConfirmPassword.setError("password mismatch");
+            }
+            else if(mTelephone.getText().toString().length() != 10) {
+                mTelephone.setError("invalid number");
+            }
+            else {
+                String username = mUsername.getText().toString();
+                Toast.makeText(signUp.this, "Sign up successful", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(signUp.this, HomeActivity.class);
+                intent.putExtra("username", username);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivityForResult(intent, 0);
+            }
         }
     }
 }
