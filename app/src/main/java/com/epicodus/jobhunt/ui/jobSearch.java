668f820ff1c5ba5d325d68c2dfd576209076332a
dicksonparlma <dicksonparlma@gmail.com>
2019-05-21 13:00:24 +0300

    inflated the logout in the home screen

--- app/src/main/java/com/epicodus/jobhunt/ui/jobSearch.java
+++ app/src/main/java/com/epicodus/jobhunt/ui/jobSearch.java
@@ -3,11 +3,15 @@ package com.epicodus.jobhunt.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.epicodus.jobhunt.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,7 +32,28 @@ public class jobSearch extends AppCompatActivity implements View.OnClickListener
         mJobs.setOnClickListener(this);
         mChat.setOnClickListener(this);
     }
+    //menu inflation//
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //logout from firebase//
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+    }
 
+    //
     @Override
     public void onClick(View v) {
         if(v == mCompany){
