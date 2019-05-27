f224bc73a583d41a893ba20be93a71141cb7d6bb
dicksonparlma <dicksonparlma@gmail.com>
2019-05-27 07:46:53 +0300

    added an itemTouchHelperAdapter

--- app/src/main/java/com/epicodus/jobhunt/util/ItemTouchHelperAdapter.java
+++ app/src/main/java/com/epicodus/jobhunt/util/ItemTouchHelperAdapter.java
@@ -2,5 +2,5 @@ package com.epicodus.jobhunt.util;
 
 public interface ItemTouchHelperAdapter {
     boolean onItemMove(int fromPosition, int toPosition);
-    void onItemDismiss(int position);
+    void onItemDismiss(int position) ;
 }
