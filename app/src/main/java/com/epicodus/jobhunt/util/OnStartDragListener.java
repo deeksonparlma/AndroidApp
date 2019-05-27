085a79a57fc03980d2a52b17a035cb9ff05af405
dicksonparlma <dicksonparlma@gmail.com>
2019-05-27 07:48:54 +0300

    added onStartDragListener interface

---
+++ app/src/main/java/com/epicodus/jobhunt/util/OnStartDragListener.java
@@ -0,0 +1,7 @@
+package com.epicodus.jobhunt.util;
+
+import android.support.v7.widget.RecyclerView;
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
